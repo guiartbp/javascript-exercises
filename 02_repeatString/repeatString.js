eb1e8e258e5f90ceaf995df7dd0a590a4f5337ad
Guilherme <guiartbp@gmail.com>
2023-02-01 14:53:27 -0300

    repeat String done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+
+    if (typeof word === 'string' && typeof number === 'number' && number > 0) {
+        let wordRepeat;
+
+        for (let i = 0; i < number; i++) {
+            if ( wordRepeat === undefined){
+                wordRepeat = word; 
+            }else {
+                wordRepeat = wordRepeat + word;
+            }
+        }
+        return wordRepeat;
+    } else if (number === 0 || word === ''){
+        return '';
+    } else {
+        return 'ERROR';
+    }
 
 };
 
