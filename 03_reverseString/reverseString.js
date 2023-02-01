2f0da39cde2a37540341756ca27c00e7bb1897d3
Guilherme <guiartbp@gmail.com>
2023-02-01 15:47:03 -0300

    reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    
+    if (word === ''){
+        return ''
+    } else {
+        let reverseWord;
+            for (let i = word.length; i >= 0; i--){
+                if (reverseWord === undefined) {
+                    reverseWord = word[i];
+                }else {
+                    reverseWord = reverseWord + word[i]
+                }
+            }
+        return reverseWord;
+    }
+    return false;
 };
 
 // Do not edit below this line
