f7df32b3e8d8a13312f27e70f027a798b3b82b0c
Guilherme <guiartbp@gmail.com>
2023-02-09 15:53:50 -0300

    leapYears done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let centYear = year % 400;
+    let divYear = year % 4;
+    let byCent = year % 100;
+
+    if (year > 100 && centYear === 0) {
+        return true;
+    } else if (byCent != 0 && divYear === 0) {
+        return true;
+    } else {
+        return false
+    }
+
 
 };
 
