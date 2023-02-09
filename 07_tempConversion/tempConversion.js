058adff77759042e65c7f5ffc91b36fa78ff071b
Guilherme <guiartbp@gmail.com>
2023-02-09 16:07:13 -0300

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let form = (fahrenheit - 32) * (5/9);
+  let rounded = Math.round(form * 10 ) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let form = (celsius * (9/5)) + 32;
+  let rounded = Math.round(form * 10 ) / 10
+  return rounded;
 };
 
 // Do not edit below this line
