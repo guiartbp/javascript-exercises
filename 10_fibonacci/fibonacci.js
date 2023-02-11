05f596c0f72db9a1eea411384cd846c904058380
Guilherme <guiartbp@gmail.com>
2023-02-11 09:24:16 -0300

    Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let number = parseInt(n);
+    
+    if (number < 0) {
+        return "OOPS"
+    } else if (number < 2) {
+        return number;
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2)
+    }
 };
 
 // Do not edit below this line
