5ba5d2f0d22907da2ca1f83c6107748ec04554f6
Guilherme <guiartbp@gmail.com>
2023-02-11 10:14:29 -0300

    findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let ageOldest = 0;
+    let objOldest;
+
+    for (const person of people){
+        if(person.hasOwnProperty('yearOfDeath')){
+
+            let agePerson = person['yearOfDeath'] - person['yearOfBirth']
+
+            if(agePerson > ageOldest){
+            ageOldest = agePerson;
+            objOldest = person;
+            }
+
+        } else {
+
+            let year =  new Date().getFullYear()
+            let agePerson = year - person['yearOfBirth']
+            
+            if(agePerson > ageOldest){
+                ageOldest = agePerson;
+                objOldest = person;
+            }
+        }
+    }
+        
+    return objOldest;
 
 };
 
