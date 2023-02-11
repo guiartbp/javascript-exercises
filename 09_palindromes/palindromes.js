d75f3782c1439af89630d2713797c90e7cadadba
Guilherme <guiartbp@gmail.com>
2023-02-11 06:06:08 -0300

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
 
+    phrase = phrase.toLowerCase().replace(/[/\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let palindromeLength = phrase.length;
+    let palindrome = "";
+
+    for(i = palindromeLength - 1; i >= 0; i--) {
+        palindrome = palindrome + phrase[i];
+    }
+    if(palindrome === phrase){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
